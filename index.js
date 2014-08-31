250fc92a0c2efb99d2d3e4965822fb01946e8424
Jordan Harband <ljharb@gmail.com>
2014-08-31 00:03:43 -0700

    Use is-generator-function instead of is-generator to detect generator functions

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-let isGen = require('is-generator');
+let isGeneratorFunction = require('is-generator-function');
 
 module.exports = skipGenerator;
 
 function* skipGenerator(gen, n) {
-  gen = isGen.fn(gen)? gen() : gen;
+  gen = isGeneratorFunction(gen)? gen() : gen;
   for (let x of gen) {
     if (n === 0) yield x;
     else --n;
